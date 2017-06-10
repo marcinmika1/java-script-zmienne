d32d3738040b175b1f439765af00eab2b26a3941
Marcin Mika <marcinmika@hotmail.com>
2017-06-10 09:54:19 +0200

    nadanie zmiennych i wyswietelenie w konsoli

---
+++ js/zmienne.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/*var nazwaZmiennej;
+
+var nazwaZmiennej=wartoscZmiennej;*/
+
+
+var imie;
+var imieMeskie = "Marcin";
+var wiek = 29;
+var kolor ="czerwony";
+ 
+console.log(imie);
+console.log(wiek);
+console.log(kolor);
+
+imie = "Sergio";
+
+console.log(imie);
+
+imie = imieMeskie; 
+
+console.log(imie);
+console.log(wiek);
+console.log(kolor);
+
+
